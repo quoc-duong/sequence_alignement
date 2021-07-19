b731ccdf111319a5df779eaca53b9eb77aa2a636
Quoc Duong Nguyen <quocduon1999@gmail.com>
2021-07-19 17:04:07 +0200

    fix positional arguments

--- src/bw-build.py
+++ src/bw-build.py
@@ -2,15 +2,16 @@ import argparse
 
 def parse_options():
     parser = argparse.ArgumentParser(description='Search subsequence in a genome following the strategy of the bowtie tool')
-    parser.add_argument("infile", help="Input file containing the sequence to process", action='store_true')
-    parser.add_argument("outfile", help="Output file", action='store_true')
-    parser.add_argument("f", help="index creation frequence", action='store_true')
+    parser.add_argument("infile", help="Input file containing the sequence to process")
+    parser.add_argument("outfile", help="Output file")
+    parser.add_argument("f", help="index creation frequence")
     parser.add_argument("--compress", dest='compress', help="Compress option", action='store_true')
     return parser.parse_args()
 
 def bw_build():
     args = parse_options()
+    print(args.infile)
     print("Hello there !")
 
 if __name__ == "__main__":
-    bw_build()
+    bw_build(
