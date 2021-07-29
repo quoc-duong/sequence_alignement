7e47d8f50cc68319abd1f1070120d864f8b40f39
Quoc Duong Nguyen <quoc-duong.nguyen@epita.fr>
2021-07-29 06:46:54 +0200

    Implement progressive bwt option

--- src/bw-build.py
+++ src/bw-build.py
@@ -1,4 +1,5 @@
 import argparse
+import random
 
 def get_all_rotations(s):
     return [s[i:] + s[:i] for i in range(len(s))]
@@ -9,7 +10,7 @@ def parse_options():
     parser.add_argument("outfile", help="Output file")
     parser.add_argument("f", help="Index creation frequence", type=int)
     parser.add_argument("--compress", dest='compress', help="Compress bwt", action='store_true')
-    parser.add_argument("--progressive", help="Construire l’index basé sur k graines", type=int)
+    parser.add_argument("--progressive", help="Construire l’index basé sur k graines", type=int, metavar='k')
     return parser.parse_args()
 
 def create_output(args, bw_transform_str, positions_index):
@@ -57,20 +58,58 @@ def get_positions_index(sorted_list, frequency):
 def bw_build():
     args = parse_options()
 
-    print(args.progressive)
-
     with open(args.infile, "r") as f:
         lines = f.readlines()[1:]
 
     sequence = "".join(lines).replace('\n', '') + '$'
 
-    all_rotations_sorted = sorted(get_all_rotations(sequence))
+    if args.progressive:
+        bw_transform_str = ""
 
-    positions_index = get_positions_index(all_rotations_sorted, args.f)
+        current_f_count = 0
+        positions_index = []
 
-    last_col_str = [rotation[-1:] for rotation in all_rotations_sorted]
 
-    bw_transform_str = "".join(last_col_str)
+        sample_indexes = random.sample(range(len(sequence)), args.progressive)
+        splitters = sorted(sequence[i:] + sequence[:i] for i in sample_indexes)
+
+        min_char = chr(0)
+        max_char = chr(255)
+
+        splitters.insert(0, min_char)
+        splitters.append(max_char)
+
+        for i in range(len(splitters) - 1):
+            bucket = []
+
+            # Create bucket between 2 splitters
+            for j in range(len(sequence)):
+                rotation = sequence[j:] + sequence[:j]
+
+                if splitters[i] <= rotation < splitters[i + 1]:
+                    bucket.append(rotation)
+
+            bucket = sorted(bucket)
+
+            last_col_str = [rotation[-1:] for rotation in bucket]
+
+            bw_transform_str += ''.join(last_col_str)
+
+            # Get position indexes
+            for j in range(len(bucket)):
+                if current_f_count == 0:
+                    positions_index.append(str(len(sequence) - bucket[j].index('$') - 1))
+
+                current_f_count = (current_f_count + 1) % args.f
+
+    else:
+        all_rotations_sorted = sorted(get_all_rotations(sequence))
+
+        positions_index = get_positions_index(all_rotations_sorted, args.f)
+
+        last_col_str = [rotation[-1:] for rotation in all_rotations_sorted]
+
+        bw_transform_str = "".join(last_col_str)
 
     create_output(args, bw_transform_str, positions_index)
 
